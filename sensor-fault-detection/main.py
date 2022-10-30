d246061e42ed424cbd4002983d51fb92e46d4a3b
Shivam Jha <shivamjhads@gmail.com>
2022-10-30 03:41:30 +0100

    sensor-fault-detection project

--- sensor-fault-detection/main.py
+++ sensor-fault-detection/main.py
@@ -6,5 +6,10 @@ from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == '__main__':
+    
     training_pipeline = TrainPipeline()
     training_pipeline.run_pipeline()
+
+    
+    # mongodb_client = MongoDBClient()
+    # print(mongodb_client.database.list_collection_names())
