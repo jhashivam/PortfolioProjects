9b7961668465b4c6bf699ca401dbc526d0c8a38a
Shivam Jha <shivamjhads@gmail.com>
2022-10-30 03:29:56 +0100

    sensor-fault-detection project

--- sensor-fault-detection/sensor/configuration/mongo_db_connection.py
+++ sensor-fault-detection/sensor/configuration/mongo_db_connection.py
@@ -11,7 +11,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                # mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 mongo_db_url  = "mongodb+srv://shivamjha:mongodb@cluster0.kzqhnhm.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
