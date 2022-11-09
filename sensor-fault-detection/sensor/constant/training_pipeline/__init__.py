cc24e8a62bbcb563e4189303f0f826baa387418c
Shivam Jha <shivamjhads@gmail.com>
2022-11-09 21:17:26 +0100

    sensor-fault-detection project

--- sensor-fault-detection/sensor/constant/training_pipeline/__init__.py
+++ sensor-fault-detection/sensor/constant/training_pipeline/__init__.py
@@ -48,11 +48,11 @@ DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 Model Trainer ralated constant start with MODE TRAINER VAR NAME
 """
 
-MODEL_TRAINER_DIR_NAME: str = "model_trainer"
-MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
-MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
-MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"     # model directory
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model" # trained model directory
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"     # trained model name
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6     # accuracy of model
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05     
 
 
 """
